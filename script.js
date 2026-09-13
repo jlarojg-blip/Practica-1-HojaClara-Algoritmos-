55697164c1a69a92bdf2f2522162b104796d0ad0
Josue Laroj <jlarojg@miumg.edu.gt>
2026-09-13 17:50:46 -0600

    Se Agrega la funcion para pider agregar valores dentro de las celdas

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 console.log("HojaClara iniciado");
 
-const filas = 15; 
+const filas = 15;
 const columnas = 10;
+
+
 
 const hoja = document.getElementById("hoja");
 
@@ -10,32 +12,93 @@ const hoja = document.getElementById("hoja");
 const tabla = document.createElement("table");
 
 
+
 const encabezado = document.createElement("tr");
 
 const esquina = document.createElement("th");
+
 
 esquina.textContent = "";
 
 encabezado.appendChild(esquina);
 
-for (let i = 0 ; i < columnas; i++) {
+for (let i = 0; i < columnas; i++) {
+
     const th = document.createElement("th");
+
     th.textContent = String.fromCharCode(65 + i);
+
     encabezado.appendChild(th);
 }
+
 tabla.appendChild(encabezado);
 
-for (let fila = 1 ; fila <=  filas; fila++) {
+
+for (let fila = 1; fila <= filas; fila++) {
+
     const tr = document.createElement("tr");
+
     const numeroFila = document.createElement("th");
     numeroFila.textContent = fila;
+
     tr.appendChild(numeroFila);
 
     for (let columna = 0; columna < columnas; columna++) {
+
         const td = document.createElement("td");
+            td.dataset.fila = fila - 1;
+   
+            td.dataset.columna = columna;
+
+
+            td.addEventListener("click", function () {
+
+    const campo = document.createElement("input");
+
+    campo.value = datos[td.dataset.fila][td.dataset.columna];
+
+    td.textContent = "";
+
+    campo.addEventListener("keydown", function (event) {
+
+        if (event.key === "Enter") {
+            datos[td.dataset.fila][td.dataset.columna] = campo.value;
+            td.textContent = campo.value;
+        }
+
+    });
+
+    campo.addEventListener("blur", function () {
+    datos[td.dataset.fila][td.dataset.columna] = campo.value;
+    td.textContent = campo.value;
+});
+
+    td.appendChild(campo);
+
+    campo.focus();
+
+});
+
         tr.appendChild(td);
     }
-           tabla.appendChild(tr);
+
+    tabla.appendChild(tr);
 }
+
+
+const datos = [];
+
+for (let fila = 0; fila < filas; fila++) {
+
+    const nuevaFila = [];
+
+    for (let columna = 0; columna < columnas; columna++) {
+
+        nuevaFila.push("");
+    }
+
+    datos.push(nuevaFila);
+}
+
 
 hoja.appendChild(tabla);
